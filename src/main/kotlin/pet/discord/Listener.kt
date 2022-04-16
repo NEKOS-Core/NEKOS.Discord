34290dd8b6223f58106123b5a8e93513ef1491e0
diademiemi <emilia@diademiemi.me>
2022-04-16 15:07:35 +0200

    Add date to event

--- src/main/kotlin/pet/discord/Listener.kt
+++ src/main/kotlin/pet/discord/Listener.kt
@@ -11,12 +11,14 @@ import pet.nekos.discord.entities.DiscordChannel
 import pet.nekos.discord.entities.DiscordMessage
 import pet.nekos.discord.entities.DiscordGuild
 
+import java.util.Date
+
 class Listener : ListenerAdapter() {
     override fun onMessageReceived(e: MessageReceivedEvent) {
         var user = DiscordUser(e.author.getName(), e.member?.getEffectiveName() as String, "TEMPORARY")
         var guild = DiscordGuild(e.message.guild.name, e.message.guild)
         var channel = DiscordChannel(e.message.channel.name, guild, e.message.guildChannel)
         var message = DiscordMessage(e.message.contentRaw, user, channel, e.message, Discord())
-        Discord().getServer().serverManager.fireEvent(MessageEvent(message))
+        Discord().getServer().serverManager.fireEvent(MessageEvent(message, Date()))
     }
 }
