efdf39af503dd4477d3898816a0fb59717e42383
diademiemi <emilia@diademiemi.me>
2022-04-22 15:35:53 +0200

    Constructor for message

--- src/main/kotlin/pet/discord/entities/DiscordMessage.kt
+++ src/main/kotlin/pet/discord/entities/DiscordMessage.kt
@@ -1,18 +1,22 @@
 package pet.nekos.discord.entities
 
 import pet.nekos.api.message.ChatMessage
+import pet.nekos.discord.Discord
 
 import net.dv8tion.jda.api.entities.Message as JDAMessage
-
-import pet.nekos.discord.Discord
 
 class DiscordMessage (
     content: String,
     user: DiscordUser,
     channel: DiscordChannel,
+    service: Discord,
     var _jdamessage: JDAMessage,
-    var _service: Discord
-) : ChatMessage(content, user, channel, _service) {
+) : ChatMessage(content, user, channel, service) {
+
+    constructor(message: JDAMessage) : this(
+        message.contentRaw, 
+        DiscordUser(message.author)) { }
+
     override fun reply(content: String): Boolean {
         _jdamessage.reply(content).queue()
         return true
