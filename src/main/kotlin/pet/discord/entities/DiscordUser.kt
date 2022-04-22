8df5e2b2910e7f06b208bde3382de4b70f9e9549
diademiemi <emilia@diademiemi.me>
2022-04-22 12:02:40 +0200

    Add user constructors

--- src/main/kotlin/pet/discord/entities/DiscordUser.kt
+++ src/main/kotlin/pet/discord/entities/DiscordUser.kt
@@ -1,9 +1,27 @@
 package pet.nekos.discord.entities
 
+import net.dv8tion.jda.api.entities.User as JDAUser
+import net.dv8tion.jda.api.entities.Member as JDAMember
+
 import pet.nekos.api.user.User
 
 class DiscordUser (
     name: String,
     nickname: String = name,
-    hash: String
-) : User(name, nickname, hash){ }
+    hash: String,
+    _jdauser: JDAUser
+) : User(name, nickname, hash){ 
+    
+    constructor(user: JDAUser) : this(
+        user.getName(), 
+        user.getName(), 
+        "TEMPORARY",
+        user) { }
+    
+    constructor(member: JDAMember) : this(
+        member.getUser().getName(), 
+        member.getEffectiveName() 
+        "TEMPORARY",
+        member.getUser()) { }
+
+}
