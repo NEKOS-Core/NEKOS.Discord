2559c4023106a49b869905b182c09974836f94d5
diademiemi <emilia@diademiemi.me>
2022-04-22 15:35:36 +0200

    Constructor for guild

--- src/main/kotlin/pet/discord/entities/DiscordGuild.kt
+++ src/main/kotlin/pet/discord/entities/DiscordGuild.kt
@@ -1,19 +1,26 @@
 package pet.nekos.discord.entities
 
 import pet.nekos.api.guild.Guild
-import pet.nekos.api.channel.Channel
+import pet.nekos.api.channel.GuildChannel
 
 import net.dv8tion.jda.api.entities.Guild as JDAGuild
 import net.dv8tion.jda.api.entities.GuildChannel as JDAGuildChannel
+
+import pet.nekos.discord.entities.DiscordGuildChannel
 
 class DiscordGuild(
     name: String,
     var _jdaguild: JDAGuild
 ) : Guild(name) {
-    override fun getChannels(): Array<Channel> {
-        var channels = mutableListOf<Channel>()
+
+    constructor(guild: JDAGuild) : this(
+        guild.getName(),
+        guild) { }
+
+    override fun getChannels(): Array<GuildChannel> {
+        var channels = mutableListOf<GuildChannel>()
         for (gc in _jdaguild.getChannels(true)) {
-            channels.add(DiscordChannel(gc.getName(), this, gc))
+            channels.add(DiscordGuildChannel(gc))
         }
         return channels.toTypedArray()
     }
