76f1fc19f0115e2806e476889813c839fa04645c
diademiemi <emilia@diademiemi.me>
2022-04-22 15:54:09 +0200

    Use the new constructors

--- src/main/kotlin/pet/discord/Listener.kt
+++ src/main/kotlin/pet/discord/Listener.kt
@@ -6,19 +6,12 @@ import pet.nekos.api.message.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-import pet.nekos.discord.entities.DiscordUser
-import pet.nekos.discord.entities.DiscordChannel
 import pet.nekos.discord.entities.DiscordMessage
-import pet.nekos.discord.entities.DiscordGuild
 
 import java.util.Date
 
 class Listener : ListenerAdapter() {
     override fun onMessageReceived(e: MessageReceivedEvent) {
-        var user = DiscordUser(e.author.getName(), e.member?.getEffectiveName() as String, "TEMPORARY")
-        var guild = DiscordGuild(e.message.guild.name, e.message.guild)
-        var channel = DiscordChannel(e.message.channel.name, guild, e.message.guildChannel)
-        var message = DiscordMessage(e.message.contentRaw, user, channel, e.message, Discord())
-        Discord().getServer().serverManager.fireEvent(MessageEvent(message, Date()))
+        Discord().getServer().serverManager.fireEvent(MessageEvent(DiscordMessage(e.message), Date()))
     }
 }
