a6c7713f92537f86e9ccbf2add5542e7bd281c38
diademiemi <emilia@diademiemi.me>
2022-04-24 14:10:34 +0200

    Fire events the new way

--- src/main/kotlin/pet/discord/Listener.kt
+++ src/main/kotlin/pet/discord/Listener.kt
@@ -1,17 +1,27 @@
 package pet.nekos.discord
 
 import pet.nekos.api.event.message.MessageEvent
-import pet.nekos.api.message.Message
+import pet.nekos.api.entities.Entity
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-import pet.nekos.discord.entities.DiscordMessage
+import pet.nekos.discord.entities.*
 
 import java.util.Date
 
 class Listener : ListenerAdapter() {
     override fun onMessageReceived(e: MessageReceivedEvent) {
-        Discord().getServer().serverManager.fireEvent(MessageEvent(DiscordMessage(e.message), Date()))
+        var entities = mutableSetOf<Entity>(
+            DiscordMessage(e.message), 
+            DiscordUser(e.author, e.member), 
+            DiscordChannel(e.channel), 
+        )
+        if (e.isFromGuild) {
+            entities.add(DiscordGuild(e.guild))
+            entities.add(DiscordGuildChannel(e.guildChannel))
+        }
+
+        Discord().getServer().serverManager.fireEvent(MessageEvent(), *entities.toTypedArray()) 
     }
 }
